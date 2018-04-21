10a7a2525520b8af1fa8c18b4c8d99924d01fc7a
Nikolay Panayotov <nickpanaiotov@gmail.com>
2018-04-21 15:52:30 +0300

    Migrated test to ES6 async/await approach.

--- test/remittance.js
+++ test/remittance.js
@@ -1,124 +1,99 @@
-var Remittance = artifacts.require("./Remittance.sol");
-var keccak256 = require('js-sha3').keccak256;
+let Remittance = artifacts.require("./Remittance.sol");
+let keccak256 = require('js-sha3').keccak256;
 
 
 contract("Remittance", function (accounts) {
 
-    var limitDuration = 3;
+    let limitDuration = 3;
 
-    var alice = accounts[0];
-    var carol = accounts[1];
+    let alice = accounts[0];
+    let carol = accounts[1];
 
-    var contract;
+    let contract;
 
-    beforeEach(function () {
-        return Remittance.new(limitDuration, {from: alice})
-            .then(function (instance) {
-                contract = instance;
-            });
+    beforeEach(async () => {
+        contract = await Remittance.new(limitDuration, {from: alice});
     });
 
+    it('everyone should be able to submit transaction', async () => {
+        let wei = 10;
+        let password = "0x" + keccak256("pesho+gosho");
+        let result = await contract.submitTransaction.call(carol, 1, password, {from: alice, value: wei});
 
-    it('everyone should be able to submit transaction', function () {
-        var wei = 10;
-        var password = "0x" + keccak256("pesho+gosho");
-
-        return contract.submitTransation.call(carol, 1, password, {from: alice, value: wei})
-            .then(function (wasSubmitted) {
-                assert.strictEqual(wasSubmitted, true, "Unable to submit valid transaction!");
-            })
+        assert.strictEqual(result, true, "Unable to submit valid transaction!");
     });
 
-    it('submitter should be able to get deadline for the submitted transaction', function () {
-        var wei = 10;
-        var password = "0x" + keccak256("pesho+gosho");
+    it('submitter should be able to get deadline for the submitted transaction', async () => {
+        let wei = 10;
+        let password = "0x" + keccak256("pesho+gosho");
 
-        return contract.submitTransation(carol, 1, password, {from: alice, value: wei})
-            .then(function (tnx) {
-                return contract.getDeadline("pesho+", "gosho", {from: alice})
-                    .then(function (deadline) {
-                        var blockNumber = web3.eth.blockNumber;
-                        assert.isBelow(blockNumber, deadline.toString(), "Unable to submit valid transaction!");
-                    });
-            })
+        await contract.submitTransaction(carol, 1, password, {from: alice, value: wei});
+        let deadline = await contract.getDeadline("pesho+", "gosho", {from: alice});
+        let blockNumber = web3.eth.blockNumber;
+
+        assert.isBelow(blockNumber, deadline.toString(), "Unable to submit valid transaction!");
     });
 
-    it('submit transaction should fail when transaction is submitted without a wei', function () {
-        var wei = 0;
-        var password = "0x" + keccak256("pesho+gosho");
+    it('submit transaction should fail when transaction is submitted without a wei', async () => {
+        let wei = 0;
+        let password = "0x" + keccak256("pesho+gosho");
 
-        return contract.submitTransation.call(carol, 1, password, {from: alice, value: wei})
-            .then(function (wasSubmitted) {
-                assert.fail('should have thrown before');
-            })
-            .catch(function (reason) {
+        try {
+            await contract.submitTransaction.call(carol, 1, password, {from: alice, value: wei});
+            assert.fail('exception should have been thrown before this line');
+        } catch (error) {
 
-            })
+        }
     });
 
-    it('submit transaction should fail when contract duration is less than the duration submitted in the transaction', function () {
-        var wei = 0;
-        var password = "0x" + keccak256("pesho+gosho");
+    it('submit transaction should fail when contract duration is less than the duration submitted in the transaction', async () => {
+        let wei = 0;
+        let password = "0x" + keccak256("pesho+gosho");
 
-        return contract.submitTransation.call(carol, limitDuration + 1, password, {from: alice, value: wei})
-            .then(function (wasSubmitted) {
-                assert.fail('should have thrown before');
-            })
-            .catch(function (reason) {
-
-            })
+        try {
+            await contract.submitTransaction.call(carol, limitDuration + 1, password, {from: alice, value: wei});
+            assert.fail('exception should have been thrown before this line');
+        } catch (error) {
+        }
     });
 
-    it('receiver can withdraw if the deadline is met and the passwords are correct', function () {
-        var wei = 10;
-        var password = "0x" + keccak256("pesho+gosho");
+    it('receiver can withdraw if the deadline is met and the passwords are correct', async () => {
+        let wei = 10;
+        let password = "0x" + keccak256("pesho+gosho");
 
-        return contract.submitTransation(carol, 2, password, {from: alice, value: wei})
-            .then(function (tnx) {
-                return contract.withdraw.call("pesho+", "gosho", {from: carol})
-                    .then(function (succeed) {
-                        assert.strictEqual(true, succeed, "Unable to withdraw!");
-                    });
-            })
+        await contract.submitTransaction(carol, 2, password, {from: alice, value: wei});
+        let succeed = await contract.withdraw.call("pesho+", "gosho", {from: carol});
+
+        assert.strictEqual(true, succeed, "Unable to withdraw!");
     });
 
-    it('submitter should be able withdraw if the deadline is not met and the passwords are correct', function () {
-        var wei = 10;
-        var password = "0x" + keccak256("pesho+gosho");
+    it('submitter should be able to withdraw after deadline with correct passwords', async () => {
+        let wei = 10;
+        let password = "0x" + keccak256("pesho+gosho");
 
-        return contract.submitTransation(carol, 1, password, {from: alice, value: wei})
-            .then(function (tnx) {
-                // Submit one dummy transaction to block++;
-                return contract.submitTransation(carol, 1, "invalidPassword", {from: alice, value: 1});
-            }).then(function (tnx) {
-                return contract.withdraw("pesho+", "gosho", {from: alice})
-                    .then(function (tnx) {
-                    })
-                    .catch(function (reason) {
-                        return contract.withdraw.call("pesho+", "gosho", {from: alice})
-                            .then(function (succeed) {
-                                assert.strictEqual(true, succeed, "Unable to withdraw!");
-                            })
-                    });
-            });
+        await contract.submitTransaction(carol, 2, password, {from: alice, value: wei});
+        await contract.submitTransaction(carol, 1, "whatever-password", {from: alice, value: 1});
+
+        try {
+            await contract.withdraw("pesho+", "gosho", {from: alice});
+            assert.fail('exception should have been thrown before this line');
+        } catch (error) {
+            await contract.withdraw("pesho+", "gosho", {from: alice});
+        }
     });
 
-    it('submitter should not be able to withdraw if the deadline is not met and the passwords are correct', function () {
-        var wei = 10;
-        var password = "0x" + keccak256("pesho+gosho");
+    it('submitter should not be able to withdraw before the deadline with correct passwords', async () => {
+        let wei = 10;
+        let password = "0x" + keccak256("pesho+gosho");
 
-        return contract.submitTransation(carol, 1, password, {from: alice, value: wei})
-            .then(function (tnx) {
-                return contract.withdraw.call("pesho+", "gosho", {from: alice})
-                    .then(function (tnx) {
-                    })
-                    .catch(function (reason) {
-                        return contract.getDeadline("pesho+", "gosho", {from: alice})
-                            .then(function (deadline) {
-                                var blockNumber = web3.eth.blockNumber;
-                                assert.isBelow(blockNumber, deadline.toString(), "Deadline is met already!");
-                            });
-                    });
-            });
+        try {
+            await contract.submitTransaction(carol, 2, password, {from: alice, value: wei});
+            await contract.withdraw("pesho+", "gosho", {from: alice});
+        } catch(error) {
+            let deadline = await contract.getDeadline("pesho+", "gosho", {from: alice});
+
+            let blockNumber = web3.eth.blockNumber;
+            assert.isBelow(blockNumber, deadline.toString(), "Deadline is met already!");
+        }
     });
 });
